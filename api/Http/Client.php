b2b1543b49500bbd2676ce1fab3be1a49f199462
Michael Sowah <michael.t.sowah@gmail.com>
2025-05-14 05:24:36 +0000

    Improve code formatting and remove unnecessary blank lines in Client.php

--- api/Http/Client.php
+++ api/Http/Client.php
@@ -52,7 +52,7 @@ class Client
 
     /**
      * Send a POST request
-     * 
+     *
      * @param string $url URL to request
      * @param array $options Request options
      * @return HttpResponse
@@ -65,7 +65,7 @@ class Client
 
     /**
      * Send a PUT request
-     * 
+     *
      * @param string $url URL to request
      * @param array $options Request options
      * @return HttpResponse
@@ -78,7 +78,7 @@ class Client
 
     /**
      * Send a DELETE request
-     * 
+     *
      * @param string $url URL to request
      * @param array $options Request options
      * @return HttpResponse
@@ -91,7 +91,7 @@ class Client
 
     /**
      * Send a PATCH request
-     * 
+     *
      * @param string $url URL to request
      * @param array $options Request options
      * @return HttpResponse
@@ -104,7 +104,7 @@ class Client
 
     /**
      * Send an HTTP request
-     * 
+     *
      * @param string $method HTTP method
      * @param string $url URL to request
      * @param array $options Request options
@@ -115,39 +115,39 @@ class Client
     {
         // Merge default options with request-specific options
         $options = array_merge($this->defaultOptions, $options);
-        
+
         // Build URL with query parameters
         if (!empty($options['query'])) {
             $separator = (strpos($url, '?') === false) ? '?' : '&';
             $url .= $separator . http_build_query($options['query']);
         }
-        
+
         // Initialize cURL
         $ch = curl_init();
-        
+
         // Set common cURL options
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, $options['timeout']);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $options['connect_timeout']);
-        
+
         // SSL verification
         if ($options['verify'] === false) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         }
-        
+
         // Set method-specific options
         switch (strtoupper($method)) {
             case 'GET':
                 break;
-                
+
             case 'POST':
                 curl_setopt($ch, CURLOPT_POST, true);
                 $this->setRequestBody($ch, $options);
                 break;
-                
+
             case 'PUT':
             case 'DELETE':
             case 'PATCH':
@@ -156,11 +156,11 @@ class Client
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
                 $this->setRequestBody($ch, $options);
                 break;
-                
+
             default:
                 throw new HttpException("Unsupported HTTP method: $method");
         }
-        
+
         // Set request headers
         $headers = [];
         if (!empty($options['headers'])) {
@@ -168,19 +168,19 @@ class Client
                 $headers[] = "$name: $value";
             }
         }
-        
+
         // Add content type header if sending JSON
         if (isset($options['json']) && !isset($options['headers']['Content-Type'])) {
             $headers[] = 'Content-Type: application/json';
         }
-        
+
         if (!empty($headers)) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         }
-        
+
         // Capture response headers
         $responseHeaders = [];
-        curl_setopt($ch, CURLOPT_HEADERFUNCTION, function($curl, $header) use (&$responseHeaders) {
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION, function ($curl, $header) use (&$responseHeaders) {
             $len = strlen($header);
             $header = explode(':', $header, 2);
             if (count($header) < 2) { // ignore invalid headers
@@ -189,7 +189,7 @@ class Client
             $responseHeaders[trim($header[0])] = trim($header[1]);
             return $len;
         });
-        
+
         // If sink is set, write to file
         if (!empty($options['sink'])) {
             $fp = fopen($options['sink'], 'w');
@@ -198,33 +198,33 @@ class Client
             }
             curl_setopt($ch, CURLOPT_FILE, $fp);
         }
-        
+
         // Execute request
         $responseBody = curl_exec($ch);
         $responseCode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
         $error = curl_error($ch);
         $errorCode = curl_errno($ch);
-        
+
         // Close file pointer if sink was set
         if (!empty($options['sink']) && isset($fp)) {
             fclose($fp);
         }
-        
+
         // Close cURL handle
         curl_close($ch);
-        
+
         // Handle errors
         if ($errorCode !== 0) {
             throw new HttpException("cURL error ($errorCode): $error", $errorCode);
         }
-        
+
         // Create response object
         $response = new HttpResponse(
             $responseCode,
             $responseHeaders,
             $responseBody !== false ? $responseBody : ''
         );
-        
+
         // For sink option, return response with empty body
         if (!empty($options['sink'])) {
             return new HttpResponse(
@@ -233,13 +233,13 @@ class Client
                 ''
             );
         }
-        
+
         return $response;
     }
-    
+
     /**
      * Set request body according to options
-     * 
+     *
      * @param \CurlHandle $ch cURL handle
      * @param array $options Request options
      */
@@ -254,13 +254,13 @@ class Client
             curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
             return;
         }
-        
+
         // Then form params
         if (!empty($options['form_params'])) {
             curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($options['form_params']));
             return;
         }
-        
+
         // Finally raw body
         if (isset($options['body'])) {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $options['body']);
