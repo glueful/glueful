16f7d00d9aa4885bc768de2d7fa2d088acce0b41
Michael Sowah <michael.t.sowah@gmail.com>
2025-05-26 06:52:41 +0000

    style(routes): format resource routes for improved readability

--- routes/resource.php
+++ routes/resource.php
@@ -8,23 +8,23 @@ $resourceController = new ResourceController();
 $request = new Request();
 
 // Resource routes
-Router::group('/', function() use ($resourceController, $request) {
-    Router::get('/{resource}', function(array $params) use ($resourceController, $request) {
+Router::group('/', function () use ($resourceController, $request) {
+    Router::get('/{resource}', function (array $params) use ($resourceController, $request) {
         $queryParams = $request->getQueryParams();
         return $resourceController->get($params, $queryParams);
     });
-    Router::post('/{resource}', function(array $params) use ($resourceController, $request) {
+    Router::post('/{resource}', function (array $params) use ($resourceController, $request) {
         $postData = Request::getPostData();
         return $resourceController->post($params, $postData);
     });
 
-    Router::put('/{resource}/{uuid}', function(array $params) use ($resourceController, $request) {
+    Router::put('/{resource}/{uuid}', function (array $params) use ($resourceController, $request) {
         $putData = Request::getPostData();
         $putData['uuid'] = $params['uuid'];
         return $resourceController->put($params, $putData);
     });
 
-    Router::delete('/{resource}/{uuid}', function(array $params) use ($resourceController, $request) {
+    Router::delete('/{resource}/{uuid}', function (array $params) use ($resourceController, $request) {
         return $resourceController->delete($params);
     });
 }, requiresAuth: true);
